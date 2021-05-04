0c0d7668d49b79976ca87ce0cf881ace452ff5da
danielboots <you@example.com>
2021-05-04 16:46:52 +0100

    set up initial page and events id slug

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>this is the about page welcome! </p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+}
