94858e9c738d73cf335a0cb748f196c256e01939
danielboots <you@example.com>
2021-05-05 13:58:17 +0100

    added static props fetching data from API

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>INTIMACY RECORDS</h1>
+      <h1>Latest</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
