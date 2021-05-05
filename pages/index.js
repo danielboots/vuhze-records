675dcb46b08fec1ed8c125c3dd870cc2d03c6183
danielboots <you@example.com>
2021-05-05 14:50:24 +0100

    added staticpaths to slug.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
+import Link from "next/link";
 
 export default function HomePage({ events }) {
   return (
@@ -10,6 +11,11 @@ export default function HomePage({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">All events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -18,7 +24,7 @@ export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   };
 }
