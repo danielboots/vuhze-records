0c4b544dedf003b180edd2bfe6313916a3d53b40
danielboots <you@example.com>
2021-05-05 13:30:06 +0100

    added event slugs and api calls for events and single event

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 import styles from "@/styles/Layout.module.css";
 
 // SEO
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -14,6 +18,9 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
 
       <Header />
+
+      {/* use router to only show hero showcase on front page */}
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
 
       <Footer />
---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Showcase.module.css";
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to Vuhze Records</h1>
+      <h2>Find the Latest Releases</h2>
+    </div>
+  );
+}
