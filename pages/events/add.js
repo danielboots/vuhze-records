9dad572de3b5f1672cc12d82483c0b14327974c0
danielboots <you@example.com>
2021-05-06 17:37:10 +0100

    working on add event page

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,11 +1,14 @@
+// import { parseCookies } from "@/helpers/index";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/Link";
+import Link from "next/link";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 
-function AddEventPage() {
+export default function AddEventPage({ token }) {
   const [values, setValues] = useState({
     name: "",
     performers: "",
@@ -13,14 +16,42 @@ function AddEventPage() {
     address: "",
     date: "",
     time: "",
-    name: "",
     description: "",
   });
 
   const router = useRouter();
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit");
+
+    // Validation
+    const hasEmptyFields = Object.values(values).some(
+      (element) => element === ""
+    );
+
+    if (hasEmptyFields) {
+      toast.error("Please fill in all fields");
+    }
+
+    const res = await fetch(`${API_URL}/events`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(values),
+    });
+
+    if (!res.ok) {
+      if (res.status === 403 || res.status === 401) {
+        toast.error("No token included");
+        return;
+      }
+      toast.error("Something Went Wrong");
+    } else {
+      const evt = await res.json();
+      router.push(`/events/${evt.slug}`);
+    }
   };
 
   const handleInputChange = (e) => {
@@ -32,7 +63,7 @@ function AddEventPage() {
     <Layout title="Add New Event">
       <Link href="/events">Go Back</Link>
       <h1>Add Event</h1>
-
+      <ToastContainer />
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.grid}>
           <div>
@@ -114,4 +145,12 @@ function AddEventPage() {
   );
 }
 
-export default AddEventPage;
+// export async function getServerSideProps({ req }) {
+//   const { token } = parseCookies(req);
+
+//   return {
+//     props: {
+//       token,
+//     },
+//   };
+// }
