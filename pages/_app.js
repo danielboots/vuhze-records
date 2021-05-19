f1f9ca96576b896ac744c170ab00dc7db4d9c2ef
danielboots <you@example.com>
2021-05-19 13:30:58 +0100

    initialised Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import { AuthProvider } from '@/context/AuthContext'
-import '../styles/globals.css'
+import { AuthProvider } from "@/context/AuthContext";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
