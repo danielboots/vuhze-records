5295f413700fd09bdab06c97031feb78a2209d32
danielboots <you@example.com>
2021-05-24 18:39:01 +0100

    button links on main page

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function HomePage({ events, artists, releases }) {
     <Layout>
       <Aboutblock />
 
-      <h3>Artists - our artists</h3>
+      <h3>Artists</h3>
 
       {artists.length === 0 && <h3>No Artists to show</h3>}
 
@@ -24,6 +24,12 @@ export default function HomePage({ events, artists, releases }) {
           {artist.genre}
         </p>
       ))}
+
+      {artists.length > 0 && (
+        <Link href="/artists">
+          <a className="btn-secondary">View All Artists</a>
+        </Link>
+      )}
 
       {/* {artists.map((artist) => (
         <ArtistItem key={artist.id} artist={artist} />
@@ -37,19 +43,27 @@ export default function HomePage({ events, artists, releases }) {
 
       <hr />
 
-      <h3>Latest Releases</h3>
+      <h3>Latest Release</h3>
 
       {releases.length === 0 && <h3>No Releases to show</h3>}
 
       {releases.map((release) => (
-        <p key={release.id} release={release}>
-          <strong>{release.trackname}</strong> <br />
-        </p>
+        <div>
+          <p key={release.id} release={release}>
+            <strong>{release.trackname}</strong> <br />
+          </p>
+          <p>{release.writeup}</p>
+        </div>
       ))}
+      {releases.length > 0 && (
+        <Link href="/releases">
+          <a className="btn-secondary">View All Releases</a>
+        </Link>
+      )}
 
       <hr />
 
-      <h3>Upcoming Events</h3>
+      <h3>Events</h3>
       {events.length === 0 && <h3>No events to show</h3>}
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
@@ -69,8 +83,8 @@ export default function HomePage({ events, artists, releases }) {
 export async function getServerSideProps() {
   const [eventRes, artistRes, releaseRes] = await Promise.all([
     await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`),
-    await fetch(`${API_URL}/artists/`),
-    await fetch(`${API_URL}/releases/`),
+    await fetch(`${API_URL}/artists?_limit=3`),
+    await fetch(`${API_URL}/releases?_limit=3`),
   ]);
   const [events, artists, releases] = await Promise.all([
     eventRes.json(),
